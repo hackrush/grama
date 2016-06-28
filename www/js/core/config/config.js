f2e7d339928669d86906b54c3af68197e538e44e
Denzel Wamburu <denzjoseph@gmail.com>
2016-06-28 21:03:48 +0300

    Fix backend url

--- www/js/core/config/config.js
+++ www/js/core/config/config.js
@@ -2,7 +2,7 @@
  * Core configuration.
  *
  * @author    Team A  {@link http://hackrush.github.io}
- * @copyright Copyright (c) 2015, Team A 
+ * @copyright Copyright (c) 2015, Team A
  * @license   The MIT License {@link http://opensource.org/licenses/MIT}
  */
 (function () {
@@ -63,5 +63,5 @@
         .module('app.core')
         .config(onConfig)
         .run(onRun)
-        .constant('SERVER_API_URL', 'http://127.0.0.1:8082'); //192.168.0.100 - 172.20.10.3
+        .constant('SERVER_API_URL', 'http://127.0.0.1:8082'); //Replace With your url here
 })();
